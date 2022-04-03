2fedf78c4111a56320f35897717d406f3a66b685
cat-toy-floof <owenfward23@gmail.com>
2022-04-03 17:57:17 -0400

    we made black pegs

--- index.js
+++ index.js
@@ -14,5 +14,24 @@ function codemaker() {
   return secretCode;
 }
 
-let secretCode = codemaker() 
+let secretCode = codemaker()
 console.log(secretCode)
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question(`What's your guess?`, guess => {
+  console.log(`Your guess is ${guess}!`)
+  readline.close()
+  guess = guess.split(" ").map(num => {
+    return parseInt(num)
+  })
+  let blackpegs=0
+  for (let i = 0; i < 4; i += 1) {
+    if(guess[i] == secretCode[i]) {
+      blackpegs += 1
+    }
+  }
+  console.log(`${blackpegs} black pegs`)
+})
